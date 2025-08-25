0c2c3ac7320120fe7a23d70ee90afbf67a96eebc
giovanasilverio <giovanasilveriopereiratp@gmail.com>
2025-08-25 14:22:32 -0300

    sign e msg error

--- src/app/core/components/auth/pages/sign/sign.component.ts
+++ src/app/core/components/auth/pages/sign/sign.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../../services/auth.service';
 
 @Component({
   selector: 'app-sign',
   templateUrl: './sign.component.html',
-  styleUrl: './sign.component.scss'
+  styleUrls: ['./sign.component.scss']
 })
-export class SignComponent {
+export class SignComponent implements OnInit {
 
+  public formAuth!: FormGroup; // será definido no ngOnInit
+
+  constructor(private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) {}
+
+  public msgError: string = '';
+
+  ngOnInit(): void {
+    this.formAuth = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+   public submitForm() {
+    if (this.formAuth.valid) {
+      this.authService
+        .sign({
+          email: this.formAuth.value.email,
+          password: this.formAuth.value.password,
+        })
+        .subscribe({
+          next: (res) => res,
+          error: (e) => (this.msgError = e),
+        });
+    }
+  }
 }
